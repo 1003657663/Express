f4079d8a41ed45c2f0bb2202fb2b73259e3ee034
chao <1003657663@qq.com>
2016-04-12 23:50:01 +0800

    login fragment add

---
+++ ExtraceIm/src/main/java/extrace/ui/main/IndexFragment.java
@@ -0,0 +1,74 @@
+package extrace.ui.main;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import zxing.util.CaptureActivity;
+
+/**
+ * Created by songchao on 16/4/4.
+ */
+public class IndexFragment extends Fragment {
+
+    private Button meButton;
+    private FragmentManager fm;
+    private ImageButton cameraButton;
+    private ImageButton messageButton;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.index_fragment,container,false);
+
+        cameraButton = (ImageButton) view.findViewById(R.id.index_top_bar_camera);
+        messageButton = (ImageButton) view.findViewById(R.id.index_top_bar_message);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCamera();
+            }
+        });
+
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        fm = getFragmentManager();
+
+        meButton = (Button) view.findViewById(R.id.me_button);
+        meButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = fm.beginTransaction();
+
+                LoginFragment loginFragment = new LoginFragment();
+                transaction.replace(R.id.fragment_container_layout, loginFragment);
+                transaction.addToBackStack("index");
+                transaction.commit();
+            }
+        });
+        return view;
+    }
+
+    private void startCamera(){
+        Intent intent = new Intent();
+        intent.putExtra("Action","Captrue");
+        intent.setClass(getActivity(), CaptureActivity.class);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.out.println("");
+    }
+}
