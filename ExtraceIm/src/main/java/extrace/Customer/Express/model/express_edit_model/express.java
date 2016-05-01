1f9c415061750fc392116b83481b7d4af03c4e5a
AbbyLiming <15136526149@163.com>
2016-05-01 20:46:03 +0800

    寄件修改

--- ExtraceIm/src/main/java/extrace/Customer/Express/model/express_edit_model/express.java
+++ ExtraceIm/src/main/java/extrace/Customer/Express/model/express_edit_model/express.java
@@ -2,11 +2,10 @@ package extrace.Customer.Express.model.express_edit_model;
 
 import android.app.Activity;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import extrace.Customer.Express.presenter.express_edit_presenter.expressPresenter;
 import extrace.net.VolleyHelper;
-import extrace.ui.main.R;
+
 
 /**
  * Created by 黎明 on 2016/4/16.
@@ -18,42 +17,27 @@ public class express extends VolleyHelper implements express_edit_model {
     public express(Activity activity, expressPresenter expressPresenter) {
         super(activity);
         this.expressPresenter = expressPresenter;
-        url =activity.getResources().getString(R.string.base_url)+activity.getResources().getString(R.string.SendExpress);
+      // url =activity.getResources().getString(R.string.base_url)+activity.getResources().getString(R.string.SendExpress);
     }
 
     @Override
     public void newExpress(int customerId,int senderID, int receiverID) {
-        JSONObject jsonObject = new JSONObject();
+        url="http://182.254.214.97:8080/REST/Domain/prepareSendExpress/customerId/"+customerId+"/sendAddressId/"+senderID+"/recAddressId/"+receiverID;
         try {
-            jsonObject.put("customerId",customerId);
-            jsonObject.put("senderID", senderID);
-            jsonObject.put("receiverID", receiverID);
-            doJson(url, VolleyHelper.POST, jsonObject);
-        } catch (JSONException e) {
+            doJson(url, VolleyHelper.GET, null);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Override
     public void onDataReceive(Object jsonOrArray) {
-            try {
-                JSONObject jsonObject=(JSONObject)jsonOrArray;
-                String ID=jsonObject.getString("ID");
-                if(!ID.equals(""))
-                        expressPresenter.onSuccess(ID);
-                else
-                        expressPresenter.onFail();
-                }
-             catch (JSONException e) {
-                e.printStackTrace();
-                expressPresenter.onFail();
-            }
-        }
-
-
+        String ID = (String) jsonOrArray;
+        expressPresenter.onSuccess(ID);
+    }
     @Override
     public void onError(String errorMessage) {
-        expressPresenter.onFail();
+        expressPresenter.onFail("error");
     }
 
 }
