5b9f714863d3d9ab673719e2dc2e012981d73b95
宋超 <songchao@songchaodeMacBook-Pro.local>
2016-05-02 15:42:48 +0800

    增加查快递界面和动画效果，topbar增加有textView

--- ExtraceIm/src/main/java/extrace/user/search/SearchExpressFragment.java
+++ ExtraceIm/src/main/java/extrace/user/search/SearchExpressFragment.java
@@ -1,14 +1,20 @@
 package extrace.user.search;
 
+import android.animation.ObjectAnimator;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import extrace.ui.main.R;
 
@@ -21,6 +27,9 @@ public class SearchExpressFragment extends Fragment implements View.OnClickListe
     private TextView searchText;
     private ImageView leftArrow;
     private LinearLayout searchContain;
+    private RelativeLayout leftContain;
+    private Text topbarTitle;
+    private ImageView topbarRight;
 
     private SearchExpressPresenter expressPresenter;
 
@@ -29,18 +38,32 @@ public class SearchExpressFragment extends Fragment implements View.OnClickListe
         View view = inflater.inflate(R.layout.user_express_search,container,false);
         searchText = (TextView) view.findViewById(R.id.express_search_id);
         leftArrow = (ImageView) view.findViewById(R.id.express_search_left_image);
+        leftContain = (RelativeLayout) view.findViewById(R.id.express_search_arrow);
         searchContain = (LinearLayout) view.findViewById(R.id.express_search_containt);
+        topbarTitle = (Text) view.findViewById(R.id.top_bar_center_text);
+        topbarRight = (ImageView) view.findViewById(R.id.top_bar_right_img);
+        topbarRight.setImageDrawable(getActivity().getResources().getDrawable(R.mipmap.map));
 
+        view.findViewById(R.id.top_bar_left_img).setOnClickListener(this);
         expressPresenter = new SearchExpressPresenterImpl(getActivity(),this);
 
         getBundle();
+
+        onRequestSuccess();
         return view;
     }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-
+            case R.id.top_bar_left_img:
+                getFragmentManager().popBackStack();
+                break;
+            case R.id.top_bar_right_img:
+                break;
+            case R.id.top_bar_right_text:
+                break;
         }
     }
 
@@ -50,12 +73,15 @@ public class SearchExpressFragment extends Fragment implements View.OnClickListe
     @Override
     public void getBundle() {
         Bundle bundle = getArguments();
-        searchID = bundle.getString("searchID");//获取运单号码
+        if(bundle!=null) {
+            searchID = bundle.getString("searchID");//获取运单号码
+        }
     }
 
     @Override
     public void init() {
         searchText.setText(searchID);
+        topbarTitle.setTextContent(searchID+"物流信息");
     }
 
     @Override
@@ -76,7 +102,64 @@ public class SearchExpressFragment extends Fragment implements View.OnClickListe
      */
     @Override
     public void onRequestSuccess() {
+        int i=10;
+        for(i = 10;i>0;i--) {//循环读取每一条数据
+            RelativeLayout relativeLayout = (RelativeLayout) LinearLayout.inflate(getActivity(), R.layout.user_express_search_item, null);
+            TextView pointText = (TextView) relativeLayout.findViewById(R.id.express_address_point);
+            TextView timeText = (TextView) relativeLayout.findViewById(R.id.express_point_date);
 
+
+
+            searchContain.addView(relativeLayout);
+        }
+
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] params) {
+                try {
+                    Thread.sleep(500);
+                    publishProgress();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                int height = leftContain.getMeasuredHeight();
+                startAni(height);
+            }
+        };
+        asyncTask.execute();
+    }
+
+    /**
+     * 开始箭头动画
+     */
+    private void startAni(int height){
+        LeftArrow leftArrowNew = new LeftArrow(leftArrow);
+        ObjectAnimator.ofInt(leftArrowNew,"height",height).setDuration(2000).start();
+
+    }
+
+    /**
+     * 重写一个有getter和setter的类，实现动画效果
+     */
+    class LeftArrow{
+        ImageView leftArrow;
+        LeftArrow(ImageView leftArrow){
+            this.leftArrow = leftArrow;
+        }
+        public int getHeight(){
+            return leftArrow.getLayoutParams().height;
+            //return leftArrow.getMeasuredHeight();
+        }
+        public void setHeight(int height){
+            leftArrow.getLayoutParams().height = height;
+            leftArrow.requestLayout();
+        }
     }
 
     private void arrowAnimation(){
