809248f39071f89d7317e78ed7db68a83b236eb3
AbbyLiming <15136526149@163.com>
2016-05-08 16:09:52 +0800

    5、8

--- ExtraceIm/src/main/java/extrace/model/ExpressInfo.java
+++ ExtraceIm/src/main/java/extrace/model/ExpressInfo.java
@@ -47,6 +47,7 @@ public class ExpressInfo implements Parcelable {
         Acc1 = in.readString();
         Acc2 = in.readString();
     }
+
     public static final Creator<ExpressInfo> CREATOR = new Creator<ExpressInfo>() {
         @Override
         public ExpressInfo createFromParcel(Parcel in) {
@@ -59,134 +60,134 @@ public class ExpressInfo implements Parcelable {
         }
     };
 
+    public double getWeight() {
+        return weight;
+    }
 
-        public double getWeight() {
-            return weight;
-        }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
-        public void setWeight(double weight) {
-            this.weight = weight;
-        }
+    public String getAcc1() {
+        return Acc1;
+    }
 
-        public String getAcc1() {
-            return Acc1;
-        }
+    public void setAcc1(String acc1) {
+        Acc1 = acc1;
+    }
 
-        public void setAcc1(String acc1) {
-            Acc1 = acc1;
-        }
+    public String getGetTime() {
+        return GetTime;
+    }
 
-        public String getGetTime() {
-            return GetTime;
-        }
+    public void setGetTime(String getTime) {
+        GetTime = getTime;
+    }
 
-        public void setGetTime(String getTime) {
-            GetTime = getTime;
-        }
+    public String getID() {
+        return ID;
+    }
 
-        public String getID() {
-            return ID;
-        }
+    public void setID(String ID) {
+        this.ID = ID;
+    }
 
-        public void setID(String ID) {
-            this.ID = ID;
-        }
+    public String getAcc2() {
+        return Acc2;
+    }
 
-        public String getAcc2() {
-            return Acc2;
-        }
+    public void setAcc2(String acc2) {
+        Acc2 = acc2;
+    }
 
-        public void setAcc2(String acc2) {
-            Acc2 = acc2;
-        }
+    public double getInsuFee() {
+        return InsuFee;
+    }
 
-        public double getInsuFee() {
-            return InsuFee;
-        }
+    public void setInsuFee(double insuFee) {
+        InsuFee = insuFee;
+    }
 
-        public void setInsuFee(double insuFee) {
-            InsuFee = insuFee;
-        }
+    public String getRadd() {
+        return radd;
+    }
 
-        public String getRadd() {
-            return radd;
-        }
+    public void setRadd(String radd) {
+        this.radd = radd;
+    }
 
-        public void setRadd(String radd) {
-            this.radd = radd;
-        }
+    public String getOutTime() {
+        return OutTime;
+    }
 
-        public String getOutTime() {
-            return OutTime;
-        }
+    public void setOutTime(String outTime) {
+        OutTime = outTime;
+    }
 
-        public void setOutTime(String outTime) {
-            OutTime = outTime;
-        }
+    public String getRaddinfo() {
+        return raddinfo;
+    }
 
-        public String getRaddinfo() {
-            return raddinfo;
-        }
+    public void setRaddinfo(String raddinfo) {
+        this.raddinfo = raddinfo;
+    }
 
-        public void setRaddinfo(String raddinfo) {
-            this.raddinfo = raddinfo;
-        }
+    public String getRname() {
+        return rname;
+    }
 
-        public String getRname() {
-            return rname;
-        }
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
 
-        public void setRname(String rname) {
-            this.rname = rname;
-        }
+    public String getRtel() {
+        return rtel;
+    }
 
-        public String getRtel() {
-            return rtel;
-        }
+    public void setRtel(String rtel) {
+        this.rtel = rtel;
+    }
 
-        public void setRtel(String rtel) {
-            this.rtel = rtel;
-        }
+    public String getSadd() {
+        return sadd;
+    }
 
-        public String getSadd() {
-            return sadd;
-        }
+    public void setSadd(String sadd) {
+        this.sadd = sadd;
+    }
 
-        public void setSadd(String sadd) {
-            this.sadd = sadd;
-        }
+    public String getSaddinfo() {
+        return saddinfo;
+    }
 
-        public String getSaddinfo() {
-            return saddinfo;
-        }
+    public void setSaddinfo(String saddinfo) {
+        this.saddinfo = saddinfo;
+    }
 
-        public void setSaddinfo(String saddinfo) {
-            this.saddinfo = saddinfo;
-        }
+    public String getSname() {
+        return sname;
+    }
 
-        public String getSname() {
-            return sname;
-        }
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
 
-        public void setSname(String sname) {
-            this.sname = sname;
-        }
+    public String getStel() {
+        return stel;
+    }
 
-        public String getStel() {
-            return stel;
-        }
+    public void setStel(String stel) {
+        this.stel = stel;
+    }
 
-        public void setStel(String stel) {
-            this.stel = stel;
-        }
+    public double getTranFee() {
+        return TranFee;
+    }
 
-        public double getTranFee() {
-            return TranFee;
-        }
+    public void setTranFee(double tranFee) {
+        TranFee = tranFee;
+    }
 
-        public void setTranFee(double tranFee) {
-            TranFee = tranFee;
-        }
 
     @Override
     public int describeContents() {
@@ -197,19 +198,38 @@ public class ExpressInfo implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(ID);
         dest.writeString(sname);
-        dest.writeString(sadd);
         dest.writeString(stel);
+        dest.writeString(sadd);
         dest.writeString(saddinfo);
-        dest.writeString(radd);
-        dest.writeString(rtel);
-        dest.writeString(raddinfo);
         dest.writeString(rname);
+        dest.writeString(rtel);
+        dest.writeString(radd);
+        dest.writeString(raddinfo);
         dest.writeString(GetTime);
         dest.writeString(OutTime);
-        dest.writeDouble(InsuFee);
+        dest.writeDouble(weight);
         dest.writeDouble(TranFee);
+        dest.writeDouble(InsuFee);
         dest.writeString(Acc1);
         dest.writeString(Acc2);
-        dest.writeDouble(weight);
     }
+
+
+
+
+    /*expressInfo.setAcc1(jsonObject.getString("acc1"));
+            expressInfo.setAcc2(jsonObject.getString("acc2"));
+            expressInfo.setGetTime(jsonObject.getString("getTime"));
+            expressInfo.setID(jsonObject.getString("ID"));
+            expressInfo.setInsuFee(jsonObject.getDouble("insuFee"));
+            expressInfo.setOutTime(jsonObject.getString("outTime"));
+            expressInfo.setRadd(jsonObject.getString("sadd"));
+            expressInfo.setRaddinfo(jsonObject.getString("raddinfo"));
+            expressInfo.setSadd(jsonObject.getString("sadd"));
+            expressInfo.setSaddinfo(jsonObject.getString("saddinfo"));
+            expressInfo.setRname(jsonObject.getString("rname"));
+            expressInfo.setSname(jsonObject.getString("sname"));
+            expressInfo.setRtel(jsonObject.getString("rtel"));
+            expressInfo.setStel(jsonObject.getString("stel"));
+            expressInfo.setWeight(jsonObject.getDouble("weight"));*/
 }
