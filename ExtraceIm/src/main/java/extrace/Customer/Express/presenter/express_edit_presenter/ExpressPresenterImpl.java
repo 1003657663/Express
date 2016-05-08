216e345943d191cd8caccff7d13fb5d614f7fb96
AbbyLiming <15136526149@163.com>
2016-05-08 14:03:42 +0800

    ExpressInfo SchangetoP res update

---
+++ ExtraceIm/src/main/java/extrace/Customer/Express/presenter/express_edit_presenter/ExpressPresenterImpl.java
@@ -0,0 +1,33 @@
+package extrace.Customer.Express.presenter.express_edit_presenter;
+
+import extrace.Customer.Express.model.ExpressEdit.ExpressEditModel;
+import extrace.Customer.Express.model.ExpressEdit.ExpressEditModelImpl;
+import extrace.Customer.Express.view.express_edit_view.ExpressEditFragmentView;
+/**
+ * Created by 黎明 on 2016/4/16.
+ */
+public class ExpressPresenterImpl implements ExpressPresenter {
+    ExpressEditFragmentView express_edit_fragmentView;
+    ExpressEditModel express_editModel;
+
+    public ExpressPresenterImpl(ExpressEditFragmentView express_edit_fragmentView) {
+        this.express_edit_fragmentView = express_edit_fragmentView;
+        express_editModel = new ExpressEditModelImpl(express_edit_fragmentView.getTheActivity(), this);
+    }
+    @Override
+    public void doNewExpress(int customerId, int send_ID, int receive_ID) {
+        express_editModel.newExpress(customerId, send_ID, receive_ID);
+    }
+
+    @Override
+    public void onSuccess(String ID) {
+        express_edit_fragmentView.onToastSuccess(ID);
+    }
+
+    @Override
+    public void onFail(String message) {
+        express_edit_fragmentView.onToastFail(message);
+    }
+
+
+}
